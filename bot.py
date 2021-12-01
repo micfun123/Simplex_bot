1b9b377407f1bc8785b948e769fab96708becb54
micfun123 <54761507+micfun123@users.noreply.github.com>
2021-12-01 11:23:19 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import aiohttp
 from os import listdir
 from os.path import isfile, join
 import json
+import easy-pil
 
 from discordLevelingSystem import DiscordLevelingSystem, RoleAward, LevelUpAnnouncement
 
@@ -203,10 +204,88 @@ async def on_message(message):
     await lvl.award_xp(amount=15, message=message)
     await client.process_commands(message)
 
-@client.command()
-async def rank(ctx):
-    data = await lvl.get_data_for(ctx.author)
-    await ctx.send(f'You are level {data.level} and your rank is {data.rank}')
+@client.command(aliases=['lvl'])
+async def rank(ctx, member:discord.Member=None):
+    if member == None:
+      data = await lvl.get_data_for(ctx.author)
+    else:
+      data = await lvl.get_data_for(member)
+
+    LEVELS_AND_XP = {
+        '0': 0,'1': 100,'2': 255,'3': 475,
+        '4': 770,'5': 1150,'6': 1625,'7': 2205,'8': 2900,'9': 3720,'10': 4675,'11': 5775,'12': 7030,
+        '13': 8450,'14': 10045,'15': 11825,'16': 13800,'17': 15980,'18': 18375,'19': 20995,'20': 23850,
+        '21': 26950,'22': 30305,'23': 33925,'24': 37820,'25': 42000,'26': 46475,'27': 51255,'28': 56350,
+        '29': 61770,'30': 67525,'31': 73625,'32': 80080,'33': 86900,'34': 94095,'35': 101675,'36': 109650,
+        '37': 118030,'38': 126825,'39': 136045,'40': 145700,'41': 155800,'42': 166355,'43': 177375,'44': 188870,
+        '45': 200850,'46': 213325,'47': 226305,'48': 239800,'49': 253820,'50': 268375,'51': 283475,'52': 299130,
+        '53': 315350,'54': 332145,'55': 349525,'56': 367500,'57': 386080,'58': 405275,'59': 425095,'60': 445550,
+        '61': 466650,'62': 488405,'63': 510825,'64': 533920,'65': 557700,'66': 582175,'67': 607355,'68': 633250,
+        '69': 659870,'70': 687225,'71': 715325,'72': 744180,'73': 773800,'74': 804195,'75': 835375,'76': 867350,
+        '77': 900130,'78': 933725,'79': 968145,'80': 1003400,'81': 1039500,'82': 1076455,'83': 1114275,'84': 1152970,
+        '85': 1192550,'86': 1233025,'87': 1274405,'88': 1316700,'89': 1359920,'90': 1404075,'91': 1449175,'92': 1495230,
+        '93': 1542250,'94': 1590245,'95': 1639225,'96': 1689200,'97': 1740180,'98': 1792175,'99': 1845195,'100': 1899250
+    }
+    
+    if member == None:
+      member = ctx.author
+    else:
+      pass
+    arank = data.xp
+    brank = LEVELS_AND_XP[f"{data.level+1}"]
+    frac = arank/brank
+    percentage = "{:.0%}".format(frac)
+    percentage = int(percentage[:-1])
+
+    user_data = {  # Most likely coming from database or calculation
+    "name": f"{member.name}",  # The user's name
+    "xp": arank,
+    "level": data.level,
+    "next_level_xp": brank,
+    "percentage": percentage,
+    "rank": data.rank
+    }
+
+    background = Editor(Canvas((934, 282), "#23272a"))
+    profile_image = load_image(str(member.avatar_url))
+    profile = Editor(profile_image).resize((150, 150)).circle_image()
+
+
+    poppins = Font.poppins(size=30)
+
+    background.rectangle((20, 20), 894, 242, "#2a2e35")
+    background.paste(profile, (50, 50))
+    background.rectangle((260, 180), width=630, height=40, fill="#484b4e", radius=20)
+    background.bar(
+        (260, 180),
+        max_width=630,
+        height=40,
+        percentage=user_data["percentage"],
+        fill="#00fa81",
+        radius=20,
+    )
+    background.text((270, 120), user_data["name"], font=poppins, color="#00fa81")
+    background.text(
+        (870, 125),
+        f"{user_data['xp']} / {user_data['next_level_xp']}",
+        font=poppins,
+        color="#00fa81",
+        align="right",
+    )
+
+    rank_level_texts = [
+        Text("Rank ", color="#00fa81", font=poppins),
+        Text(f"{user_data['rank']}", color="#1EAAFF", font=poppins),
+        Text("   Level ", color="#00fa81", font=poppins),
+        Text(f"{user_data['level']}", color="#1EAAFF", font=poppins),
+    ]
+
+    background.multicolor_text((850, 30), texts=rank_level_texts, align="right")
+
+    # send
+    background.save(f"rank{member.id}.png")
+    await ctx.send(file=discord.File(f"rank{member.id}.png"))
+    os.remove(f"rank{member.id}.png")
 
 
 @client.command()
