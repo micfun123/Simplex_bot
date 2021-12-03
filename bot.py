c49c5274cc8f99db2fc14e2b0611b51c05c4f3e8
micfun123 <54761507+micfun123@users.noreply.github.com>
2021-12-03 10:43:49 +0400

    remove duplicates

--- bot.py
+++ bot.py
@@ -88,47 +88,7 @@ async def warn(ctx, user: discord.User, *, message=None):
     message = message or "This Message is a warning"
     await discord.User.send(user, message + (f"** Warned by {ctx.message.author} From server {message.server.name}**"))
 
-@client.command(aliases=["doggo"], help = "It shows you a Dog photo as well as a fact") #shows a dog photo and a fact
-async def dog(ctx):
-   async with aiohttp.ClientSession() as session:
-      request = await session.get('https://some-random-api.ml/img/dog')
-      dogjson = await request.json()
-      # This time we'll get the fact request as well!
-      request2 = await session.get('https://some-random-api.ml/facts/dog')
-      factjson = await request2.json()
-
-   embed = discord.Embed(title="Doggo!", color=discord.Color.purple())
-   embed.set_image(url=dogjson['link'])
-   embed.set_footer(text=factjson['fact'])
-   await ctx.send(embed=embed)
-
-@client.command(help = "It shows you a cat photo as well as a fact") #shows cat photo and fact
-async def cat(ctx):
-   async with aiohttp.ClientSession() as session:
-      request = await session.get('https://some-random-api.ml/img/cat')
-      catjson = await request.json()
-      # This time we'll get the fact request as well!
-      request2 = await session.get('https://some-random-api.ml/facts/cat')
-      factjson = await request2.json()
-
-   embed = discord.Embed(title="Cat!", color=discord.Color.purple())
-   embed.set_image(url=catjson['link'])
-   embed.set_footer(text=factjson['fact'])
-   await ctx.send(embed=embed)
-
-
-@client.command(aliases=["Joke", "jokes", "Jokes"], help = "It tells a joke")  #tells a joke
-async def joke(ctx):
-   async with aiohttp.ClientSession() as session:
-      # This time we'll get the joke request as well!
-      request = await session.get('https://some-random-api.ml/joke')
-      jokejson = await request.json()
-
    
-   embed = discord.Embed(title="I know its funny", color=discord.Color.purple())
-   embed.set_footer(text=jokejson['joke'])
-   await ctx.send(embed=embed) 
-    
 
 #gets user info of user on the discord
 @client.command(aliases=["userinfo"] ,help = "Finds info about users on the discord.")
