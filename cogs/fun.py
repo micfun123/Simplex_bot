6c6a4eac59885ec80908ce5f1bae55f499d095cb
micfun123 <54761507+micfun123@users.noreply.github.com>
2021-12-20 11:47:53 +0400

    coinflip

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,11 +1,16 @@
 import discord
+import random
 from discord.ext import commands
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    @commands.command(aliases=["flip", "coin"])
+    async def coinflip(self, ctx):
+        """ Coinflip! """
+        coinsides = ["Heads", "Tails"]
+        await ctx.send(f"**{ctx.author.name}** flipped a coin and got **{random.choice(coinsides)}**!")
 
 
 def setup(bot):
