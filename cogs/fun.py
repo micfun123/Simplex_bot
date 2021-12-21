6afc671a5b747b23b3dcb898a791bc4355bda0e3
micfun123 <michaelrbparker@gmail.com>
2021-12-21 14:19:40 +0400

    .reverse

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,6 +12,14 @@ class Fun(commands.Cog):
         coinsides = ["Heads", "Tails"]
         await ctx.send(f"**{ctx.author.name}** flipped a coin and got **{random.choice(coinsides)}**!")
 
+    @commands.command()
+    async def reverse(self, ctx, *, text: str):
+        """ !poow ,ffuts esreveR
+        Everything you type after reverse will of course, be reversed
+        """
+        t_rev = text[::-1].replace("@", "@\u200B").replace("&", "&\u200B")
+        await ctx.send(f"🔁 {t_rev}")
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
