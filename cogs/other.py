18d5c93c1e64987b46b26a5e9dc08a6c6fc6c2d3
micfun123 <54761507+micfun123@users.noreply.github.com>
2021-12-29 21:22:43 +0400

    suggest

---
+++ cogs/other.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+import asyncio
+
+class utilities(commands.Cog):
+
+    @commands.command(aliases=['sug'])
+    async def suggest(self, ctx, *, suggestion):
+        sid = await self.client.fetch_channel(908969607266730005)
+        await sid.send(f"Suggestion:\n{suggestion}\n\nBy: {ctx.author.name}\nID: {ctx.author.id}")
+        await ctx.send("Thank you for you suggestion!")
+
+
+def setup(bot):
+    bot.add_cog(utilities(bot))
