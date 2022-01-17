90712da4894ec2635303e2da8dd43db4438ff3b1
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-01-17 10:30:11 +0400

    server info

--- cogs/other.py
+++ cogs/other.py
@@ -1,3 +1,7 @@
+import imp
+
+
+import discord
 from discord.ext import commands
 import asyncio
 
@@ -12,6 +16,30 @@ class utilities(commands.Cog):
         sid = await self.client.fetch_channel(908969607266730005)
         await sid.send(f"Suggestion:\n{suggestion}\n\nBy: {ctx.author.name}\nID: {ctx.author.id}")
         await ctx.send("Thank you for you suggestion!")
+
+    @commands.command()
+    async def serverinfo(self,ctx):
+        name = str(ctx.guild.name)
+        description = str(ctx.guild.description)
+
+        owner = str(ctx.guild.owner)
+        id = str(ctx.guild.id)
+        region = str(ctx.guild.region)
+        memberCount = str(ctx.guild.member_count)
+
+
+        embed = discord.Embed(
+            title=name + " Server Information",
+            description=description,
+            color=discord.Color.blue()
+        )
+        embed.set_thumbnail(url=ctx.guild.icon.url)
+        embed.add_field(name="Owner", value=owner, inline=True)
+        embed.add_field(name="Server ID", value=id, inline=True)
+        embed.add_field(name="Region", value=region, inline=True)
+        embed.add_field(name="Member Count", value=memberCount, inline=True)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
