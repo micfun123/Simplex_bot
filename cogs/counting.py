79092f58a1a52e1c2fd238867b4f4f94c44086fe
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-01-18 07:18:06 +0400

    counting

--- cogs/counting.py
+++ cogs/counting.py
@@ -16,10 +16,15 @@ async def get_counting_channel(guild):
 
 
 async def counting(msg, guild, channel, m):
+    
     try:
         msg = int(msg)
     except:
-        return
+        try:
+            msg = int(eval(msg))
+            await m.reply(msg)
+        except:
+         return    
 
     cc = await get_counting_channel(guild)
 
