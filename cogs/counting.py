7b82c295b60652fdfa3b2a762d919d9ef1092d89
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-01-28 16:05:56 +0400

    remove print

--- cogs/counting.py
+++ cogs/counting.py
@@ -110,13 +110,13 @@ class Counting(commands.Cog):
             data.append(insert)
             with open("./databases/db.json", 'w') as f:
                 json.dump(data, f, indent=4)
-            await ctx.send(data)
+            
             with open("./databases/counting.json") as f:
                 data2 = json.load(f)
             data2[f"{i.id}"] = 0
             with open("./databases/counting.json", 'w') as f:
                 json.dump(data2, f, indent=4)
-            await ctx.send(data)
+            
     
     @commands.command()
     async def setcountchannel(self, ctx, channel:discord.TextChannel):
