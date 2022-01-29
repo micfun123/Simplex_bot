4a83f02c75d7b842d1be15bfd525e531f5064dfe
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-01-29 10:13:37 +0400

    events

---
+++ cogs/event.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import json
+from datetime import datetime
+from utils import log
+
+
+async def update_activity(client):
+    await client.change_presence(activity=discord.Game(f"On {len(client.guilds)} servers! | ?help"))
+    print("Updated presence")
+
+class Events(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        cha = self.client.get_channel(925513395883606129)
+        await cha.send(embed=discord.Embed(title="Join", description=f"Joined: {guild.name}"))
+        await update_activity(self.client)
+       
+            
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self,guild):
+        await update_activity(self.client)
+        cha = self.client.get_channel(925513395883606129)
+        await cha.send(embed=discord.Embed(title="Leave", description=f"Left: {guild.name}"))
+
+def setup(client):
+    client.add_cog(Events(client))
