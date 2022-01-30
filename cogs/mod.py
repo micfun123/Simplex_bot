27f02bf22ed185bbc77791dd5e5eb2793cfbbe66
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-01-30 20:25:56 +0400

    lockdown

--- cogs/mod.py
+++ cogs/mod.py
@@ -108,7 +108,41 @@ class Moderation(commands.Cog):
         await ctx.send(f"The module '{extension}' has been unloaded successfully!")
 
 
-    
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def lockdown(self, ctx, channel: discord.TextChannel = None):
+        channel = channel or ctx.channel
+        role = ctx.guild.default_role
+
+        if role not in channel.overwrites:
+            overwrites = {
+                role: discord.PermissionOverwrite(send_messages=False)
+            }
+            await channel.edit(overwrites=overwrites)
+            if ctx.channel != channel:
+                await ctx.send(f"I have put {channel.mention} on lockdown.")
+            else:
+                await ctx.message.delete()
+            await channel.send(embed=discord.Embed(title="This channel is now under lockdown", color=discord.Colour.orange()))
+        elif channel.overwrites[role].send_messages is True or \
+                channel.overwrites[role].send_messages is None:
+            overwrites = channel.overwrites[role]
+            overwrites.send_messages = False
+            await channel.set_permissions(role, overwrite=overwrites)
+            if ctx.channel != channel:
+                await ctx.send(f"I have put {channel.mention} on lockdown.")
+            else:
+                await ctx.message.delete()
+            await channel.send(embed=discord.Embed(title="This channel is now under lockdown.", color=discord.Colour.orange()))
+        else:
+            overwrites = channel.overwrites[role]
+            overwrites.send_messages = True
+            await channel.set_permissions(role, overwrite=overwrites)
+            if ctx.channel != channel:
+                await ctx.send(f"I have removed {channel.mention} from lockdown.")
+            else:
+                await ctx.message.delete()
+            await channel.send(embed=discord.Embed(title="This channel is no longer under lockdown.", color=discord.Colour.orange()))
 
 def setup(client):
     client.add_cog(Moderation(client))
