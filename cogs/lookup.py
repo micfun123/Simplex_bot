754a6674d43ded8fac8f1624aff2df78d9045d96
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-02-03 12:05:49 +0400

    wiki look nice

--- cogs/lookup.py
+++ cogs/lookup.py
@@ -1,24 +1,39 @@
 import imp
 from pydoc import describe
+from this import s
 from discord import Embed
 import wikipedia
 from discord.ext import commands
 import json
 import os
-from tools import log
 import discord
+import imp
+import traceback
+from tools import log
 
-
+def printError(error):
+    etype = type(error)
+    trace = error.__traceback__
+    lines = traceback.format_exception(etype, error, trace)
+    traceback_text = ''.join(lines)
+    print(traceback_text)
+    log(traceback_text)
 class lookup(commands.Cog):
     def __init__(self, client): 
         self.client = client 
 
 
+
+
     @commands.command()
     async def wiki(self, ctx, *, query):
-        await ctx.send(wikipedia.summary(query, sentences=20))
-        log(query + " was looked up on wikipedia")
-
+        embed = Embed(title="Wikipedia", description="Searching for {}".format(query), color=0x00ff00)
+        page = wikipedia.summary(query, chars=1000)
+        url = wikipedia.page(query).url
+        embed.add_field(name="Description", value=page)
+        embed.add_field(name="Link", value=url ,inline=False)
+        await ctx.send(embed=embed)
+       
 
 def setup(client):
     client.add_cog(lookup(client))
