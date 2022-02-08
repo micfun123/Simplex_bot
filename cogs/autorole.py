17df91440406958009414f3f345a7e3b414efbac
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2022-02-09 11:48:06 +1300

    Commit

--- cogs/autorole.py
+++ cogs/autorole.py
@@ -7,10 +7,8 @@ class Autorole(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.group(help="Sets the autorole for the server")
+    @commands.command(help="Sets the autorole for the server")
     async def autorole(self, ctx, role:discord.Role):
-        if ctx.invoked_subcommand is None:
-            return
         role_id = role.id
         guild_id = str(ctx.guild.id)
 
@@ -24,8 +22,8 @@ class Autorole(commands.Cog):
 
         await ctx.send(embed=discord.Embed(title=f"{role.mention} has been set as the autorole for this server"))
 
-    @autorole.command()
-    async def reset(self, ctx):
+    @commands.command()
+    async def autorolereset(self, ctx):
         guild_id = str(ctx.guild.id)
 
         with open("./databases/autorole.json") as f:
@@ -37,7 +35,7 @@ class Autorole(commands.Cog):
             json.dump(data, f, indent=4)
 
         await ctx.send(embed=discord.Embed(title="Autorole reset"))
-        
+
     @commands.Cog.listener()
     async def on_member_join(self, member):
         with open("./databases/autorole.json") as f:
