90903097e928b04b3c81e471bcc63ce6899c6bf9
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-02-23 09:11:42 +0400

    Translate

--- cogs/translate.py
+++ cogs/translate.py
@@ -1,14 +1,17 @@
 import discord
-import datetime
-import random
 from discord.ext import commands
-import os
-import json
+from translate import Translator
 
 class Translate(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command(help="Translate text from one language to another. You must use the language code, not the language name. EG Arabic = ar")
+    async def translate(self, ctx, lang, *, text):
+        translator = Translator(to_lang=lang)
+        translation = translator.translate(text)
+        embed = discord.Embed(title=f'Translation', description=translation, color=0x00ff00)
+        await ctx.send(embed=embed)
 
         
 def setup(client):
