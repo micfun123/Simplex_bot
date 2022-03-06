ee3bf7836d37d88b48ab7799ce0a09d3ced31984
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-06 07:20:39 +0400

    push

--- cogs/cards.py
+++ cogs/cards.py
@@ -3,14 +3,15 @@ from datetime import datetime
 from time import sleep
 import requests
 from discord.ext import commands
+from discord.commands import slash_command
 
 
 class cards(commands.Cog):
     def __init__(self, client): 
         self.client = client 
     
-    @commands.command(aliases=['rc'])
-    async def randomcard(self, ctx):
+    @commands.command(name="randomcard", help="Gets you a random card")
+    async def randomcard_(self, ctx):
         url = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1'
         response = requests.get(url)
         data = response.json()
@@ -19,6 +20,17 @@ class cards(commands.Cog):
         embed.set_image(url=data['cards'][0]['image'])
         embed.set_footer(text="Time: " + str(datetime.now()))
         await ctx.send(embed=embed)
+
+    @slash_command(name="randomcard", help="Gets you a random card")
+    async def randomcard(self, ctx):
+        url = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1'
+        response = requests.get(url)
+        data = response.json()
+        embed = discord.Embed(title=f"Random Card", color=0x20BEFF)
+        embed.description = f"{data['cards'][0]['value']} of {data['cards'][0]['suit']}"
+        embed.set_image(url=data['cards'][0]['image'])
+        embed.set_footer(text="Time: " + str(datetime.now()))
+        await ctx.respond(embed=embed)
 
 def setup(client):
     client.add_cog(cards(client))
--- cogs/chess.py
+++ cogs/chess.py
@@ -3,14 +3,15 @@ from datetime import datetime
 from time import sleep
 import requests
 from discord.ext import commands
+from discord.commands import slash_command
 
 
 class chess(commands.Cog):
     def __init__(self, client): 
         self.client = client 
     
-    @commands.command(aliases=['coft'])
-    async def TodaysChess(self, ctx):
+    @commands.command(name = "todayschess", help = "Gets you todays chess challenge",)
+    async def todaysChess_(self, ctx):
         url = 'https://api.chess.com/pub/puzzle/random'
         response = requests.get(url)
         data = response.json()
@@ -20,6 +21,20 @@ class chess(commands.Cog):
         embed.add_field(name="url:", value=data['url'], inline=False)
         embed.set_footer(text="Time: " + str(datetime.now()))
         await ctx.send(embed=embed)
+
+
+    @slash_command(name = "todayschess", help = "Gets you todays chess challenge")
+    async def todaysChess(self, ctx):
+        url = 'https://api.chess.com/pub/puzzle/random'
+        response = requests.get(url)
+        data = response.json()
+        embed = discord.Embed(title=f"{data['title']}", color=0x20BEFF)
+        embed.set_image(url=data['image'])
+        embed.add_field(name="fen:", value=data['fen'], inline=False)
+        embed.add_field(name="url:", value=data['url'], inline=False)
+        embed.set_footer(text="Time: " + str(datetime.now()))
+        await ctx.respond(embed=embed)
+
 
 def setup(client):
     client.add_cog(chess(client))
