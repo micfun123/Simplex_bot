38954e60dd00582c84eaec208a425bb49c034bd7
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-08 13:46:59 +0400

    chess

--- cogs/chess.py
+++ cogs/chess.py
@@ -12,26 +12,26 @@ class chess(commands.Cog):
     
     @commands.command(name = "todayschess", help = "Gets you todays chess challenge",)
     async def todaysChess_(self, ctx):
-        url = 'https://api.chess.com/pub/puzzle/random'
+        url = 'https://chesspuzzle.net/Daily/Api'
         response = requests.get(url)
         data = response.json()
-        embed = discord.Embed(title=f"{data['title']}", color=0x20BEFF)
-        embed.set_image(url=data['image'])
-        embed.add_field(name="fen:", value=data['fen'], inline=False)
-        embed.add_field(name="url:", value=data['url'], inline=False)
+        embed = discord.Embed(title=f"{data['Puzzle']}", color=0x20BEFF)
+        embed.set_image(url=data['Image'])
+        embed.add_field(name="To Start:", value=data['Text'], inline=False)
+        embed.add_field(name="url:", value=data['Link'], inline=False)
         embed.set_footer(text="Time: " + str(datetime.now()))
         await ctx.send(embed=embed)
 
 
     @slash_command(name = "todayschess", help = "Gets you todays chess challenge")
     async def todaysChess(self, ctx):
-        url = 'https://api.chess.com/pub/puzzle/random'
+        url = 'https://chesspuzzle.net/Daily/Api'
         response = requests.get(url)
         data = response.json()
-        embed = discord.Embed(title=f"{data['title']}", color=0x20BEFF)
-        embed.set_image(url=data['image'])
-        embed.add_field(name="fen:", value=data['fen'], inline=False)
-        embed.add_field(name="url:", value=data['url'], inline=False)
+        embed = discord.Embed(title=f"{data['Puzzle']}", color=0x20BEFF)
+        embed.set_image(url=data['Image'])
+        embed.add_field(name="To Start:", value=data['Text'], inline=False)
+        embed.add_field(name="url:", value=data['Link'], inline=False)
         embed.set_footer(text="Time: " + str(datetime.now()))
         await ctx.respond(embed=embed)
 
