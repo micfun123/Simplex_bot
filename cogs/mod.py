b009397268e23c405256cd138697a9422ceed24f
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-11 13:00:13 +0400

    invites

--- cogs/mod.py
+++ cogs/mod.py
@@ -229,6 +229,23 @@ class Moderation(commands.Cog):
             await ctx.send('Member not found.')
 
     
+    @commands.command()
+    async def invites(self, ctx, user:discord.Member=None):
+        if user is None:
+            total_invites = 0
+            for i in await ctx.guild.invites():
+                if i.inviter == ctx.author:
+                    total_invites += i.uses
+            await ctx.send(f"You've invited {total_invites} member{'' if total_invites == 1 else 's'} to the server!")
+        else:
+            total_invites = 0
+            for i in await ctx.guild.invites():
+                if i.inviter == user:
+                    total_invites += i.uses
+
+            await ctx.send(f"{user} has invited {total_invites} member{'' if total_invites == 1 else 's'} to the server!")
+
+    
 
 def setup(client):
     client.add_cog(Moderation(client))
