c35b1a903f4c46eab8e7e3942b93ba95f3de0552
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-23 20:05:18 +0400

    logs

--- cogs/logger.py
+++ cogs/logger.py
@@ -24,23 +24,22 @@ class Moderationsettings(commands.Cog):
     #This bit not working \/   \/
 
     @commands.Cog.listener()
-    async def on_message_delete(self,message,member):
+    async def on_message_delete(self,message):
         data = await get_data()
         for i in data:
-            if i['guild_id'] == member.guild.id:
+            if i['guild_id'] == message.author.guild.id:
                 stuff = i
 
         channel = stuff['channel']
        
-
         channel = await self.client.fetch_channel(channel)
         embed = discord.Embed(
             title="{}'s message deleted.".format(message.author.name), #message.author is sender of the message
             description=message.content,
-            color="teal"
+            color=discord.Color.red()
         )
-        channel=self.get_channel(channel)
         await channel.send(embed=embed) 
+
 
 #This bit not working^^
 
