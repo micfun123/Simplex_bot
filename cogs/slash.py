8048055dad0bafb793f8c0aaa0fced001fc3b9f4
Siddhesh Zantye <siddheshadsv@icloud.com>
2022-10-07 02:02:02 +1300

    Rickroll lang + zprol support
    ctrl+c/v so not 100% sure it works yet

--- cogs/slash.py
+++ cogs/slash.py
@@ -22,6 +22,23 @@ for i in os.listdir("cogs/"):
         pass
     else:
         print(i[:-3])
+        
+class CodeInput(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.code = None
+
+        self.add_item(
+            discord.ui.InputText(
+                label="Please enter the code", style=discord.InputTextStyle.long
+            )
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        self.code = self.children[0].value
+        return await interaction.response.send_message(
+            "Running code now...", ephemeral=True
+        )
 
 class Slash(commands.Cog):
     def __init__(self, client):
@@ -166,6 +183,79 @@ class Slash(commands.Cog):
         calc = simpcalc.Calculate()
         ans = await calc.calculate(equation)
         await ctx.send(f"The equation is: {equation}\nThe answer is: {ans}")
+
+
+    @commands.slash_command()
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def zprol(self, ctx):
+        modal = CodeInput(title="Code Input")
+        await ctx.send_modal(modal)
+        await modal.wait()
+
+        if modal.code is None:
+            return await ctx.respond("Invalid Input", ephemeral=True)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "https://zprol.epicpix.ga/api/v1/run",
+                json={"code": modal.code},
+            ) as resp:
+                response = await resp.json()
+            if resp.status != 200:
+                em = discord.Embed(
+                    title="zProl",
+                    description="Something went wrong!\n(API probably had a skill issue)",
+                    color=discord.Color.blue(),
+                )
+                return await ctx.respond(embed=em)
+            
+        desc = "```\n"
+        if response["compilation"]["stdout"] != "":
+            desc += "stdout:\n" + response["compilation"]["stdout"]
+        if response["compilation"]["stderr"] != "":
+            desc += "\n\nstderr:\n" + response["compilation"]["stdout"]
+
+        em = discord.Embed(
+            title="Output",
+            color=discord.Color.blue(),
+            description=desc + "\n```",
+        )
+        if response["run"] is not None or response["run"] != "":
+            em.add_field(name="Run:", value=response["run"])
+
+        await ctx.respond(embed=em)
+
+    @commands.slash_command()
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def ricklang(self, ctx):
+        modal = CodeInput(title="Code Input")
+        await ctx.send_modal(modal)
+        await modal.wait()
+
+        if modal.code is None:
+            return await ctx.respond("Invalid Input", ephemeral=True)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "https://api.fusionsid.xyz/api/runcode",
+                json={"code": modal.code, "language": "rickroll_lang"},
+            ) as resp:
+                response = await resp.json()
+            if resp.status != 200:
+                em = discord.Embed(
+                    title="Rickroll-Lang",
+                    description="Something went wrong!\n(API probably had a skill issue)",
+                    color=discord.Color.blue(),
+                )
+                await ctx.respond(embed=em)
+
+        em = discord.Embed(
+            title="Output",
+            color=discord.Color.blue(),
+            description=f"""```\n{response['stdout']}\n```""",
+        )
+
+        await ctx.respond(embed=em)
 
 def setup(client):
     client.add_cog(Slash(client))
