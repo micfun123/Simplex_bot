b833d041a16cb92e19bde3cb40d31a454f7676d1
Michael <Michaelrbparker@gmail.com>
2022-10-19 21:25:20 +0100

    spell good

--- cogs/mclook.py
+++ cogs/mclook.py
@@ -51,7 +51,7 @@ class Minecraft(commands.Cog):
                 await ctx.send(embed=embed)
 
     @commands.slash_command(name="mcping", description= "Gets the status of a Minecraft server, Ping and player count")
-    async def mc_ping__slasj(self, ctx, server: str):
+    async def mc_ping__slash(self, ctx, server: str):
             url = f"https://api.mcsrvstat.us/2/{server}"
             response = requests.get(url)
             resp = response.json()
