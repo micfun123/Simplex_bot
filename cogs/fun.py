cb737f72099bb63d17395849e199944de5a95d30
Michael <Michaelrbparker@gmail.com>
2022-11-03 17:30:52 +0000

    slap that

--- cogs/fun.py
+++ cogs/fun.py
@@ -40,7 +40,7 @@ def slap(imageUrl):
         frames.append(frame)
 
     d = BytesIO()
-    frames[0].save(d, format="GIF", save_all=True, append_images=frames[1:], loop=0, duration=100, disposal=2)
+    frames[0].save('new.gif', append_images=frames[1:], save_all=True)
     d.seek(0)
     return d
         
