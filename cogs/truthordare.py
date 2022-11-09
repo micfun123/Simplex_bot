9d2184d5e0ec7f41b48a1725d53bb7dbd70d14e0
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-09 19:23:07 +0000

    no more table

--- cogs/truthordare.py
+++ cogs/truthordare.py
@@ -2,27 +2,34 @@ import discord
 from discord.ext import commands
 import sqlite3
 
+def enabled_check(ctx):
+    con = sqlite3.connect('databases/truthordare.db')
+    cur = con.cursor()
+    cur.execute(f"SELECT enabled FROM guilds WHERE guild_id = {ctx.guild.id}")
+    enabled = cur.fetchone()
+    if enabled[0][0] == 1:
+        return True
 
 class TruthOrDare(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def maketable(self, ctx):
-        con = sqlite3.connect('databases/truthordare.db')
-        cur = con.cursor()
-        cur.execute("CREATE table truthordare (server_id int, toggel int)")
-        con.commit()
-        con.close()
-        await ctx.send("Table created")
-        for i in self.client.guilds:
-            con = sqlite3.connect('databases/truthordare.db')
-            cur = con.cursor()
-            cur.execute("INSERT INTO truthordare VALUES (?, ?)", (i.id, 0))
-            con.commit()
-            con.close()
-        await ctx.send("Table filled")
-    
+    #@commands.command()
+    #async def maketable(self, ctx):
+    #    con = sqlite3.connect('databases/truthordare.db')
+    #    cur = con.cursor()
+    #    cur.execute("CREATE table truthordare (server_id int, toggel int)")
+    #    con.commit()
+    #    con.close()
+    #    await ctx.send("Table created")
+    #    for i in self.client.guilds:
+    #        con = sqlite3.connect('databases/truthordare.db')
+    #        cur = con.cursor()
+    #        cur.execute("INSERT INTO truthordare VALUES (?, ?)", (i.id, 0))
+    #        con.commit()
+    #        con.close()
+    #    await ctx.send("Table filled")
+    #
     #onserverjoin
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
@@ -61,6 +68,9 @@ class TruthOrDare(commands.Cog):
             con.commit()
             con.close()
             await ctx.respond("Truth or dare enabled!")
+
+    @commands.command()
+    
 
             
 
