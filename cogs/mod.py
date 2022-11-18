7994bb0b4b80cab37e38765a86c93bf5e4037740
Michael <Michaelrbparker@gmail.com>
2022-11-18 18:41:10 +0000

    antiowner messup

--- cogs/mod.py
+++ cogs/mod.py
@@ -58,15 +58,15 @@ class Moderation(commands.Cog):
     def __init__(self, client): 
         self.client = client 
 
-    @commands.is_owner()
-    @commands.command()
-    async def make_file_table_ticketrs(self, ctx):
-        con = sqlite3.connect('databases/ticket_channel_id.db')
-        cur = con.cursor()
-        cur.execute("CREATE table ticket_channel_id (userid it, channel_id int)")
-        con.commit()
-        con.close()
-        await ctx.send("Table created")
+    #@commands.is_owner()
+    #@commands.command()
+    #async def make_file_table_ticketrs(self, ctx):
+    #    con = sqlite3.connect('databases/ticket_channel_id.db')
+    #    cur = con.cursor()
+    #    cur.execute("CREATE table ticket_channel_id (userid it, channel_id int)")
+    #    con.commit()
+    #    con.close()
+    #    await ctx.send("Table created")
         
     
     @commands.command()
