da37556e588f62c13dbe9741bf77b7c6216a82b4
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-24 22:01:22 +0000

    sql

--- cogs/counting.py
+++ cogs/counting.py
@@ -4,15 +4,10 @@ from discord.ext import commands
 import json
 from simpcalc import simpcalc
 import asyncio
-calculator = simpcalc.Calculate()
+import sqlite3
 
-async def get_counting_channel(guild):
-    with open("./databases/db.json") as f:
-        data = json.load(f)
-    for i in data:
-        if i["guild_id"] == guild.id:
-            return int(i["counting_channel"])
-    return None
+
+calculator = simpcalc.Calculate()
 
 # Counting
 
@@ -24,66 +19,77 @@ async def counting(msg, guild, channel, m):
       msg = int(ans)
     except:
      return    
-
-    cc = await get_counting_channel(guild)
-
-    if cc is None:
+    con = sqlite3.connect("databases/counting.db")
+    cur = con.cursor()
+    ##get counting channel
+    countingchannel = cur.execute("SELECT counting_channel FROM counting WHERE Guild_id = ?", (guild.id,)).fetchone()[0]
+    if countingchannel is None:
         return
-    if channel.id == cc:
-        with open("./databases/counting.json") as f:
-            data = json.load(f)
-        with open('./databases/db.json') as f:
-            dataa = json.load(f)
-        for i in dataa:
-            if i['guild_id'] == guild.id:
-                if i['lastcounter'] == None:
-                    i['lastcounter'] = m.author.id
-                    break
-                elif i['lastcounter'] == m.author.id:
-                    data[f"{guild.id}"] = 0
-                    i['lastcounter'] = None
-                    await m.add_reaction("❌")
-                    em = discord.Embed(title=f"{m.author.display_name}, You ruined it!", description="Only one person at a time\nCount reset to zero")
-                    with open("./databases/counting.json", 'w') as f:
-                        json.dump(data, f, indent=4)
-                    with open("./databases/db.json", 'w') as f:
-                        json.dump(dataa, f, indent=4)
-                    return await channel.send(embed=em)
+    if countingchannel != channel.id:
+        return
+    else:
+        ##get last number
+        lastnumber = cur.execute("SELECT lastcounter FROM counting WHERE Guild_id = ?", (guild.id,)).fetchone()[0]
+        lastuser = cur.execute("SELECT last_user FROM counting WHERE Guild_id = ?", (guild.id,)).fetchone()[0]
+        highest = cur.execute("SELECT highest FROM counting WHERE Guild_id = ?", (guild.id,)).fetchone()[0]
+        if lastnumber is None:
+            cur.execute("UPDATE counting SET lastcounter = ? WHERE Guild_id = ?", (msg, guild.id))
+            con.commit()
+            con.close()
+            return
+        if msg == lastnumber + 1:
+            if lastuser == m.author.id:
+                await m.add_reaction("❌")
+                em = discord.Embed(title=f"{m.author.display_name}, You ruined it!", description="Take it in turns and stop being selfish\nCount reset to zero")
+                cur.execute("UPDATE counting SET lastcounter = ? WHERE Guild_id = ?", (0, guild.id))
+                cur.execute("UPDATE counting SET last_user = ? WHERE Guild_id = ?", (None, guild.id))
+                con.commit()
+                con.close()
+                return await channel.send(embed=em)
+            else:
+                
+                cur.execute("UPDATE counting SET lastcounter = ? WHERE Guild_id = ?", (msg, guild.id))
+                cur.execute("UPDATE counting SET last_user = ? WHERE Guild_id = ?", (m.author.id, guild.id))
+                if msg == highest+1:
+                    cur.execute("UPDATE counting SET highest = ? WHERE Guild_id = ?", (msg, guild.id))
+                    await m.add_reaction("☑")
                 else:
-                    i['lastcounter'] = m.author.id
-                    break
-                    
-        if (data[f"{guild.id}"] + 1) == msg:
-            data[f"{guild.id}"] += 1
-            await m.add_reaction("✅")
-            if msg == 69:
-                await channel.send("Nice")
-            if msg == 42:
-                await channel.send("You have found the meaning of life.")
+                    await m.add_reaction("✅")
+                con.commit()
+                con.close()
+                if msg == 69:
+                    await channel.send("nice")
+                if msg == 420:
+                    await channel.send("nice")
+                if msg == 42069:
+                    await channel.send("You have found the meaning of life.")
+                return
         else:
             await m.add_reaction("❌")
-            em = discord.Embed(title=f"{m.author.display_name}, You ruined it!", description=f"You were supposed to type `{(data[f'{guild.id}']+1)}`\nCount reset to zero")
-            i['lastcounter'] = None
-            data[f"{guild.id}"] = 0
-            await channel.send(embed=em)
-        with open("./databases/counting.json", 'w') as f:
-            json.dump(data, f, indent=4)
-        with open("./databases/db.json", 'w') as f:
-            json.dump(dataa, f, indent=4)
-
+            em = discord.Embed(title=f"{m.author.display_name}, You ruined it!", description=f"Count reset to zero. you were supposed to count {lastnumber + 1}")
+            cur.execute("UPDATE counting SET lastcounter = ? WHERE Guild_id = ?", (0, guild.id))
+            cur.execute("UPDATE counting SET last_user = ? WHERE Guild_id = ?", (None, guild.id))
+            con.commit()
+            con.close()
+            return await channel.send(embed=em)
 
 class Counting(commands.Cog):
     def __init__(self, client):
         self.client = client
-        
-    @commands.command(aliases=['num'])
-    async def numrn(self, ctx):
-        guild = ctx.guild
-        with open('./databases/counting.json') as f:
-            data = json.load(f)
-        guildid = f'{guild.id}'
-        numrn = data[guildid]
-        await ctx.send(f"Current number is {numrn}")
+
+    @commands.is_owner()
+    @commands.command()
+    async def makedbtable(self, ctx):
+        con = sqlite3.connect("./databases/counting.db")
+        cur = con.cursor()
+        cur.execute("CREATE TABLE counting (guild_id INTEGER, counting_channel INTEGER, lastcounter INTEGER,highest INTEGER, last_user INTEGER)")
+        con.commit()
+        for i in self.client.guilds:
+            cur.execute("INSERT INTO counting VALUES (?, ?, ?, ?,?)", (i.id, None, None, 0, None))
+        con.commit()
+        con.close()
+        await ctx.send("Done")
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -100,83 +106,47 @@ class Counting(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def set_num(self,ctx,nums):
-        with open("./databases/counting.json") as f:
-            data = json.load(f)
-            nums = int(nums)
-        data[f"{ctx.guild.id}"] = nums
-        with open("./databases/counting.json", 'w') as f:
-                        json.dump(data, f, indent=4)
+        con = sqlite3.connect("./databases/counting.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM counting WHERE guild_id = ?", (ctx.guild.id,))
+        data = cur.fetchone()
+        num = int(nums)
+        cur.execute("UPDATE counting SET lastcounter = ? WHERE guild_id = ?", (nums, ctx.guild.id))
+        con.commit()
+        con.close()
         await ctx.send(f"Numb set to {nums}")
-
-
-
-    # delete after usage 
-    @commands.command()
-    @commands.check(mic) # cmd can only be run by mic
-    async def sacc(self, ctx):
-        for i in self.client.guilds:
-            insert = {
-                "guild_id": i.id,
-                "counting_channel": None,
-                "lastcounter":None,
-            }
-            with open("./databases/db.json") as f:
-                data = json.load(f)
-            data.append(insert)
-            with open("./databases/db.json", 'w') as f:
-                json.dump(data, f, indent=4)
-            
-            with open("./databases/counting.json") as f:
-                data2 = json.load(f)
-            data2[f"{i.id}"] = 0
-            with open("./databases/counting.json", 'w') as f:
-                json.dump(data2, f, indent=4)
             
     
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def setcountchannel(self, ctx, channel:discord.TextChannel):
-        with open("./databases/db.json") as f:
-            data = json.load(f)
-        for i in data:
-            if i['guild_id'] == ctx.guild.id:
-                i['counting_channel'] = channel.id
-        with open('./databases/db.json', 'w') as f:
-            json.dump(data, f, indent =4)
-            await ctx.send("Done")
-
+        con = sqlite3.connect("./databases/counting.db")
+        cur = con.cursor()
+        cur.execute("UPDATE counting SET counting_channel = ? WHERE guild_id = ?", (channel.id, ctx.guild.id))
+        con.commit()
+        con.close()
+        await ctx.send(f"Counting channel set to {channel.mention}")
             
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def countingoff(self, ctx):
-            with open("./databases/db.json") as f:
-                data = json.load(f)
-            for i in data:
-                if i['guild_id'] == ctx.guild.id:
-                    i['counting_channel'] = None
-            with open('./databases/db.json', 'w') as f:
-                json.dump(data, f, indent =4)
+        con = sqlite3.connect("./databases/counting.db")
+        cur = con.cursor()
+        cur.execute("UPDATE counting SET counting_channel = ? WHERE guild_id = ?", (None, ctx.guild.id))
+        con.commit()
+        con.close()
+        await ctx.send(f"Counting channel turned off")
     
             
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        insert = {
-            "guild_id": guild.id,
-            "counting_channel": None,
-            "lastcounter":None,
-        }
-        with open("./databases/db.json") as f:
-            data = json.load(f)
-        data.append(insert)
-        with open("./databases/db.json", 'w') as f:
-            json.dump(data, f, indent=4)
-
-        with open("./databases/counting.json") as f:
-            data2 = json.load(f)
-        data2[f"{guild.id}"] = 0
-        with open("./databases/counting.json", 'w') as f:
-            json.dump(data2, f, indent=4)
-
+        con = sqlite3.connect("./databases/counting.db")
+        cur = con.cursor()
+        data =  cur.execute("SELECT * FROM counting WHERE guild_id = ?", (guild.id,)).fetchall()
+        if not data:
+            cur.execute("INSERT INTO counting VALUES (?, ?, ?, ?)", (guild.id, None, None, 0))
+            con.commit()
+        con.close()
 
 
 def setup(client):
