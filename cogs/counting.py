c5571b1599efebaeb88cc74fce47854b0e7c1611
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-27 14:48:05 +0000

    newstats

--- cogs/counting.py
+++ cogs/counting.py
@@ -87,6 +87,10 @@ async def counting(msg, guild, channel, m):
             last_user = await last_user.fetchone()
             highest = await db.execute("SELECT highest FROM counting WHERE Guild_id = ?", (guild.id,))
             highest = await highest.fetchone()
+            attemps = await db.execute("SELECT attemps FROM counting WHERE Guild_id = ?", (guild.id,))
+            attemps = await attemps.fetchone()
+            attemps = attemps[0]
+            attemps = int(attemps)
             if last_number is None:
                 await db.execute("UPDATE counting SET lastcounter = ? WHERE Guild_id = ?", (msg, guild.id))
                 await db.commit()
@@ -120,6 +124,8 @@ async def counting(msg, guild, channel, m):
                 em = discord.Embed(title=f"{m.author.display_name}, You ruined it!", description=f"Count reset to zero. you were supposed to count {last_number[0] + 1}")
                 await db.execute("UPDATE counting SET lastcounter = ? WHERE Guild_id = ?", (0, guild.id))
                 await db.execute("UPDATE counting SET last_user = ? WHERE Guild_id = ?", (None, guild.id))
+                await db.execute("UPDATE counting SET highest = ? WHERE Guild_id = ?", (0, guild.id))
+                await db.execute("UPDATE counting SET attemps = ? WHERE Guild_id = ?", (attemps+1, guild.id))
                 await db.commit()
                 return await channel.send(embed=em)
 
@@ -129,18 +135,28 @@ class Counting(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.is_owner()
+    
+
     @commands.command()
-    async def makedbtable(self, ctx):
-        con = sqlite3.connect("./databases/counting.db")
-        cur = con.cursor()
-        cur.execute("CREATE TABLE counting (guild_id INTEGER, counting_channel INTEGER, lastcounter INTEGER,highest INTEGER, last_user INTEGER)")
-        con.commit()
-        for i in self.client.guilds:
-            cur.execute("INSERT INTO counting VALUES (?, ?, ?, ?,?)", (i.id, None, 0, 0, None))
-        con.commit()
-        con.close()
-        await ctx.send("Done")
+    @commands.is_owner()
+    async def add_column(self, ctx):
+        async with aiosqlite.connect("./databases/counting.db") as db:
+            for i in self.client.guilds:
+                await db.execute("ALTER TABLE counting ADD COLUMN attemps INTEGER DEFAULT 0")
+
+    
+    #@commands.is_owner()
+    #@commands.command()
+    #async def makedbtable(self, ctx):
+    #    con = sqlite3.connect("./databases/counting.db")
+    #    cur = con.cursor()
+    #    cur.execute("CREATE TABLE counting (guild_id INTEGER, counting_channel INTEGER, lastcounter INTEGER,highest INTEGER, last_user INTEGER)")
+    #    con.commit()
+    #    for i in self.client.guilds:
+    #        cur.execute("INSERT INTO counting VALUES (?, ?, ?, ?,?)", (i.id, None, 0, 0, None))
+    #    con.commit()
+    #    con.close()
+    #    await ctx.send("Done")
 
 
     @commands.Cog.listener()
@@ -154,6 +170,32 @@ class Counting(commands.Cog):
 
     def mic(ctx):
         return ctx.author.id == 481377376475938826
+
+    @commands.slash_command(name="counting_stats", description="Get the stats of the counting channel")
+    async def counting_stats(self, ctx):
+        async with aiosqlite.connect("./databases/counting.db") as db:
+            counting_channel = await db.execute("SELECT counting_channel FROM counting WHERE Guild_id = ?", (ctx.guild.id,))
+            counting_channel = await counting_channel.fetchone()
+            if counting_channel is None:
+                return await ctx.send("There is no counting channel set up")
+            if counting_channel[0] is None:
+                return await ctx.send("There is no counting channel set up")
+            else:
+                last_number = await db.execute("SELECT lastcounter FROM counting WHERE Guild_id = ?", (ctx.guild.id,))
+                last_number = await last_number.fetchone()
+                last_user = await db.execute("SELECT last_user FROM counting WHERE Guild_id = ?", (ctx.guild.id,))
+                last_user = await last_user.fetchone()
+                highest = await db.execute("SELECT highest FROM counting WHERE Guild_id = ?", (ctx.guild.id,))
+                highest = await highest.fetchone()
+                attemps = await db.execute("SELECT attemps FROM counting WHERE Guild_id = ?", (ctx.guild.id,))
+                attemps = await attemps.fetchone()
+                embed = discord.Embed(title=f"Counting Stats for {ctx.guild.name}", description=f"Counting channel: <#{counting_channel[0]}>",color=discord.Color.green())
+                embed.add_field(name="Last number", value=last_number[0])
+                embed.add_field(name="Highest number", value=highest[0])
+                embed.add_field(name="Last user", value=f"<@{last_user[0]}>")
+                embed.add_field(name="Attemps", value=attemps[0])
+                await ctx.respond(embed=embed)
+
 
     @commands.command()
     @commands.has_permissions(administrator=True)
@@ -200,23 +242,23 @@ class Counting(commands.Cog):
             con.commit()
         con.close()
 
-    @commands.is_owner()
-    @commands.command()
-    async def countannounce(self, ctx):
-        with open("./databases/db.json", "rb") as f:
-            data = json.load(f)
-        serverssent = 0
-        #go throught list 
-        for i in data:
-            try:
-                channel = await self.client.fetch_channel(i["counting_channel"])
-                await channel.send(f"Couning has been rewriten to be more relable. Please use the new command to set the counting channel. `.setcountchannel <channel>`. This will set the channel to 0 to change the number use `.set_num <number>`. IF you find any bugs please dm the bot\n Now with a highscore marker")
-                serverssent += 1
-            except Exception as e:
-                print(e)
-                ctx.send(f"Error in {i}")
-                pass
-        await ctx.send(f"Sent to {serverssent} servers")
+    #@commands.is_owner()
+    #@commands.command()
+    #async def countannounce(self, ctx):
+    #    with open("./databases/db.json", "rb") as f:
+    #        data = json.load(f)
+    #    serverssent = 0
+    #    #go throught list 
+    #    for i in data:
+    #        try:
+    #            channel = await self.client.fetch_channel(i["counting_channel"])
+    #            await channel.send(f"Couning has been rewriten to be more relable. Please use the new command to set the counting channel. `.setcountchannel <channel>`. This will set the channel to 0 to change the number use `.set_num <number>`. IF you find any bugs please dm the bot\n Now with a highscore marker")
+    #            serverssent += 1
+    #        except Exception as e:
+    #            print(e)
+    #            ctx.send(f"Error in {i}")
+    #            pass
+    #    await ctx.send(f"Sent to {serverssent} servers")
 
 def setup(client):
     client.add_cog(Counting(client))
