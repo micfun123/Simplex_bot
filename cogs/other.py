4404fad2e4381ba3cb57f781f2801814bc5fc43c
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-12-05 20:28:38 +0000

    remove bot killer

--- cogs/other.py
+++ cogs/other.py
@@ -151,43 +151,43 @@ class utilities(commands.Cog):
         await channelbug.send(f"Bug report:\n{bug}\n\nBy: {ctx.author.name}\nID: {ctx.author.id}")
         await ctx.send("Thank you for reporting this bug!")
 
-    @commands.command(aliases=["channel_stats", "channel_health", "channel_info", "channel_information"])
-    async def channel_status(self, ctx, channel: discord.TextChannel = None):
-        if not channel:
-            channel = ctx.channel
-
-        server_id = self.client.get_guild(self.client.guilds[0].id)
-
-        embed = discord.Embed(colour=discord.Colour.orange())
-        embed.set_author(name="Channel Health:")
-
-        async with ctx.channel.typing():
-            count = 0
-            async for message in channel.history(limit=500000, after=datetime.today() - timedelta(days=100)): count += 1
-
-            if count >= 5000:
-                average = "OVER 5000!"
-                healthiness = "VERY HEALTHY"
-
-            else:
-                try:
-                    average = round(count / 100, 2)
-
-                    if 0 > server_id.member_count / average: healthiness = "VERY HEALTHY"
-                    elif server_id.member_count / average <= 5: healthiness = "HEALTHY"
-                    elif server_id.member_count / average <= 10: healthiness = "NORMAL"
-                    elif server_id.member_count / average <= 20: healthiness = "UNHEALTHY"
-                    else: healthiness = "VERY UNHEALTHY"
-
-                except ZeroDivisionError:
-                    average = 0
-                    healthiness = "VERY UNHEALTHY"
-
-            embed.add_field(name="­", value=f"# of members: {server_id.member_count}", inline=False)
-            embed.add_field(name="­", value=f'# of messages per day on average in "{channel}" is: {average}', inline=False)
-            embed.add_field(name="­", value=f"Channel health: {healthiness}", inline=False)
-
-            await ctx.send(embed=embed)
+    #@commands.command(aliases=["channel_stats", "channel_health", "channel_info", "channel_information"])
+    #async def channel_status(self, ctx, channel: discord.TextChannel = None):
+    #    if not channel:
+    #        channel = ctx.channel
+#
+    #    server_id = self.client.get_guild(self.client.guilds[0].id)
+#
+    #    embed = discord.Embed(colour=discord.Colour.orange())
+    #    embed.set_author(name="Channel Health:")
+#
+    #    async with ctx.channel.typing():
+    #        count = 0
+    #        async for message in channel.history(limit=500000, after=datetime.today() - timedelta(days=100)): count += 1
+#
+    #        if count >= 5000:
+    #            average = "OVER 5000!"
+    #            healthiness = "VERY HEALTHY"
+#
+    #        else:
+    #            try:
+    #                average = round(count / 100, 2)
+#
+    #                if 0 > server_id.member_count / average: healthiness = "VERY HEALTHY"
+    #                elif server_id.member_count / average <= 5: healthiness = "HEALTHY"
+    #                elif server_id.member_count / average <= 10: healthiness = "NORMAL"
+    #                elif server_id.member_count / average <= 20: healthiness = "UNHEALTHY"
+    #                else: healthiness = "VERY UNHEALTHY"
+#
+    #            except ZeroDivisionError:
+    #                average = 0
+    #                healthiness = "VERY UNHEALTHY"
+#
+    #        embed.add_field(name="­", value=f"# of members: {server_id.member_count}", inline=False)
+    #        embed.add_field(name="­", value=f'# of messages per day on average in "{channel}" is: {average}', inline=False)
+    #        embed.add_field(name="­", value=f"Channel health: {healthiness}", inline=False)
+#
+    #        await ctx.send(embed=embed)
 
     @commands.command(pass_context=True, aliases=['getcolor'])
     async def getcolour(self, ctx, *, colour_codes):
