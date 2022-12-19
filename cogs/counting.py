96b63c9a41bab19aaf8b0f14d5d296f53bc654a7
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-12-19 19:13:11 +0000

    base 69

--- cogs/counting.py
+++ cogs/counting.py
@@ -20,6 +20,7 @@ async def counting(msg, guild, channel, m):
     try:
         if msg.startswith("69*|"):
             msg = decode_base69(msg)
+            msg = int(msg)
         calc = simpcalc.Calculate()
         ans = await calc.calculate(msg)
         msg = int(ans)
