02401b1c069193737e1a4786c38106ed9476f3c7
Michael Parker <michaelrbparker@gmail.com>
2023-06-03 11:23:48 +0100

    latex make expressions fir the canvus

--- cogs/other.py
+++ cogs/other.py
@@ -20,6 +20,7 @@ from io import BytesIO
 from base69 import encode_base69, decode_base69
 import sympy
 import matplotlib.pyplot as plt
+import math
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 def latex_to_image(latex_expression):
@@ -386,52 +387,66 @@ class utilities(commands.Cog):
 
     @commands.slash_command(name="latex", description="Convert LaTeX to image")
     async def _latex_slash(self, ctx, expression: str):
-        # Convert the LaTeX expression to an image
         # Configure matplotlib to use the Agg backend
         plt.switch_backend("Agg")
         plt.rcParams["text.usetex"] = True
-        plt.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"   
+        plt.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"
+
+        # Calculate the required canvas size based on the length of the expression
+        expression_length = len(expression)
+        width = math.ceil(math.sqrt(expression_length))  # Round up the square root
+        height = math.ceil(expression_length / width)
 
         # Create a figure and plot the LaTeX expression
         fig, ax = plt.subplots()
-        ax.text(0.5, 0.5, f"${expression}$", fontsize=16, ha="center")  
+        ax.text(0.5, 0.5, f"${expression}$", fontsize=16, ha="center")
 
-        # Set the size of the figure
-        fig.set_size_inches(3, 1)   
+        # Set the size of the figure based on the calculated dimensions
+        fig.set_size_inches(width, height)
 
-        # save the image to a file
+        # Save the image to a file
         fig.savefig("latex.png")
-        #send the image
+
+        # Send the image
         await ctx.respond(file=discord.File("latex.png"))
-        #close the figure
+
+        # Close the figure
         plt.close(fig)
-        #delete the image
+
+        # Delete the image
         os.remove("latex.png")
 
-    @commands.command(name="latex", aliases=["tex"], help = "Convert LaTeX to image")
+
+    @commands.command(name="latex", aliases=["tex"], help="Convert LaTeX to image")
     async def _latex_(self, ctx, *, expression: str):
-        # Convert the LaTeX expression to an image
         # Configure matplotlib to use the Agg backend
         plt.switch_backend("Agg")
         plt.rcParams["text.usetex"] = True
-        plt.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"   
+        plt.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"
+
+        # Calculate the required canvas size based on the length of the expression
+        expression_length = len(expression)
+        width = math.ceil(math.sqrt(expression_length))  # Round up the square root
+        height = math.ceil(expression_length / width)
 
         # Create a figure and plot the LaTeX expression
         fig, ax = plt.subplots()
-        ax.text(0.5, 0.5, f"${expression}$", fontsize=16, ha="center")  
+        ax.text(0.5, 0.5, f"${expression}$", fontsize=16, ha="center")
 
-        # Set the size of the figure
-        fig.set_size_inches(3, 1)   
+        # Set the size of the figure based on the calculated dimensions
+        fig.set_size_inches(width, height)
 
-        # save the image to a file
+        # Save the image to a file
         fig.savefig("latex.png")
-        #send the image
+
+        # Send the image
         await ctx.send(file=discord.File("latex.png"))
-        #close the figure
+
+        # Close the figure
         plt.close(fig)
-        #delete the image
+
+        # Delete the image
         os.remove("latex.png")
-        
 
 
     #random num generator between two numbers
