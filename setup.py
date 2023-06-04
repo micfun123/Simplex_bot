c6bd0d88c5a1ea7fd0c16da04995e3b51b9509fd
Michael Parker <michaelrbparker@gmail.com>
2023-06-04 08:54:16 +0100

    set up birthday autorole and raids

--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
 #generate all the databases
+import sqlite3
+
+#rade stuff for the anti cog
+con = sqlite3.connect("databases/raids.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE raids(ServerID int, Servertoggle, raiderneed int, currentrade int)")
+con.commit()
+con.close()
+print("raids.db created")
+
+
+con = sqlite3.connect("databases/autoroles.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE autoroles(guild_id int, role_id int)")
+con.commit()
+con.close()
+print("autoroles.db created")
+
+con = sqlite3.connect("databases/server_brithdays.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE server(ServerID int, Servertoggle, birthdaychannel int,birthdaymessage text)")
+con.commit()
+con.close()
+print("server_brithdays.db created")
+con = sqlite3.connect("databases/user_brithdays.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE birthday(UsersID int, birthday)")
+con.commit()
+con.close()
+print("user_brithdays.db created")
+
+        
