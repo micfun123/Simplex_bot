904667c29402f2e57e2ad50117a3eaf6b8e9e68b
Michael Parker <michaelrbparker@gmail.com>
2023-06-28 20:32:05 +0100

    testing if bot can detect channels

--- cogs/music.py
+++ cogs/music.py
@@ -11,34 +11,47 @@ class Music(commands.Cog):
     def cog_unload(self):
         if self.voice and self.voice.is_connected():
             self.queue.clear()
-            self.voice.stop()
-            self.voice.disconnect()
+            self.bot.loop.create_task(self.voice.disconnect())
 
-    @commands.command()
-    async def join(self, ctx):
+    async def check_voice_channel(self, ctx):
         if not ctx.message.author.voice:
             await ctx.send("You are not connected to a voice channel.")
-            return
+            return False
 
+        return True
+
+    async def join_voice_channel(self, ctx):
         if self.voice and self.voice.is_connected():
             await self.voice.move_to(ctx.message.author.voice.channel)
         else:
             self.voice = await ctx.message.author.voice.channel.connect()
 
     @commands.command()
+    async def join(self, ctx):
+        if not await self.check_voice_channel(ctx):
+            return
+
+        await self.join_voice_channel(ctx)
+        await ctx.send("Joined the voice channel.")
+
+    @commands.command()
     async def leave(self, ctx):
-        if self.voice and self.voice.is_connected():
-            self.queue.clear()
-            await self.voice.stop()
-            await self.voice.disconnect()
-        else:
+        if not self.voice or not self.voice.is_connected():
             await ctx.send("I am not connected to a voice channel.")
+            return
+
+        self.queue.clear()
+        await self.voice.disconnect()
+        self.voice = None
+        await ctx.send("Left the voice channel.")
 
     @commands.command()
     async def play(self, ctx, url):
-        if not self.voice or not self.voice.is_connected():
-            await ctx.send("I am not connected to a voice channel. Use the `join` command first.")
+        if not await self.check_voice_channel(ctx):
             return
+
+        if not self.voice or not self.voice.is_connected():
+            await self.join_voice_channel(ctx)
 
         ydl_opts = {
             'format': 'bestaudio/best',
@@ -56,7 +69,7 @@ class Music(commands.Cog):
 
         if not self.voice.is_playing():
             await self.play_next(ctx)
-
+    
     async def play_next(self, ctx):
         if not self.queue:
             return
@@ -68,21 +81,25 @@ class Music(commands.Cog):
     async def pause(self, ctx):
         if self.voice and self.voice.is_playing():
             self.voice.pause()
+            await ctx.send("Playback paused.")
 
     @commands.command()
     async def resume(self, ctx):
         if self.voice and self.voice.is_paused():
             self.voice.resume()
+            await ctx.send("Playback resumed.")
 
     @commands.command()
     async def skip(self, ctx):
         if self.voice and self.voice.is_playing():
             self.voice.stop()
             await self.play_next(ctx)
+            await ctx.send("Skipped to the next song.")
 
     @commands.command()
-    async def clear_queue(self, ctx):
+    async def clear(self, ctx):
         self.queue.clear()
+        await ctx.send("Queue cleared.")
 
 def setup(bot):
     bot.add_cog(Music(bot))
