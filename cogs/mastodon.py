9c31be63af1fdc2304e6fce7e5b158792d590469
Michael Parker <michaelrbparker@protonmail.com>
2023-07-19 17:49:41 +0100

    <span class="h-card" translate="no"> removed

--- cogs/mastodon.py
+++ cogs/mastodon.py
@@ -78,6 +78,7 @@ class mastodon(commands.Cog):
                     clean_content = clean_content.replace("<span class=\"p-category\">", "")
                     clean_content = clean_content.replace("<span class=\"p-locality\">", "")
                     clean_content = clean_content.replace("<span class=\"p-region\">", "")
+                    clean_content = clean_content.replace('<span class="h-card" translate="no">', "")
                     
                     embed = discord.Embed(title=f"New post from {username}", description=clean_content, color=discord.Color.random())
                     embed.add_field(name="Link", value=f"https://mastodon.social/web/statuses/{last_post}")
